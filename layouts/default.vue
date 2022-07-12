346c73ddc919e8ccde09dac42c3d66851fdba1b9
mostafa7904 <mostafa7904@gmail.com>
2022-07-12 15:39:51 +0430

    init

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "defaultLayout",
+  data: () => ({}),
+});
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center"
+  >
+    <div class="not-found-text">
+      4
+      <img
+        class="tomato-o-img"
+        src="/png/not-found/tomato-o.png"
+        alt="tomato, not found"
+      />
+      4
+    </div>
+    <div class="mt-4">
+      {{ error.statusCode === 404 ? pageNotFound : otherError }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  data: () => ({
+    pageNotFound: "Seems like you got lost...",
+    otherError: "Something went wrong...",
+  }),
+  head() {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
+    return {
+      title,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+$size: 120px;
+.tomato-o-img {
+  margin-top: 0.1 * $size;
+  width: $size - 50px;
+  height: $size - 50px;
+}
+.not-found-text {
+  line-height: 1;
+  display: flex;
+  font-size: $size;
+  align-items: center;
+  justify-content: center;
+}
+</style>
