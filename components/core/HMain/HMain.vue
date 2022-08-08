bf198e8c6481cfed17f9582c5e207dfa79554cd4
mostafa7904 <mostafa7904@gmail.com>
2022-08-08 22:54:45 +0430

    feat(HMain): add HMain component

---
+++ components/core/HMain/HMain.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="h-main">
+    <div class="h-main__wrap">
+      <slot></slot>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HealthFreakMain",
+});
+</script>
+
+<style lang="scss">
+@import "~/assets/variables.scss";
+
+.h-main {
+  display: flex;
+  flex: 1 0 auto;
+  max-width: 100%;
+  padding: $main-padding;
+  transition: 0.2s cubic-bezier(0.4, 0, 0.2, 1);
+  &__wrap {
+    flex: 1 1 auto;
+    max-width: 100%;
+    position: relative;
+  }
+}
+</style>
